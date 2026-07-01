2a7e7682ff6993a0cfe9855f0e2c92a67bc7fe3d
Roman Stikhin <r_stikhi@live.concordia.ca>
2026-07-01 07:01:29 -0400

    numbers on fingers now

--- app/hand_tracker.py
+++ app/hand_tracker.py
@@ -28,23 +28,32 @@ def main():
 
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        mp_image = mp.Image(
-            image_format=mp.ImageFormat.SRGB,
-            data=rgb_frame
-        )
-        
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+
         result = landmarker.detect(mp_image)
-        
+
         if result.hand_landmarks:
             for hand_landmarks in result.hand_landmarks:
-                for landmark in hand_landmarks:
+                for i, landmark in enumerate(hand_landmarks):
                     h, w, _ = frame.shape
+
                     x = int(landmark.x * w)
                     y = int(landmark.y * h)
+
                     cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
-        
-        #print(result.hand_landmarks)
-        
+
+                    cv2.putText(
+                        frame,
+                        str(i),
+                        (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 255, 255),
+                        1,
+                    )
+
+        # print(result.hand_landmarks)
+
         cv2.imshow("gesture stage Hand Tracking", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
